0093e86e9abd4e0f096387774ce6aa8b74c285ad
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-22 12:20:16 +0530

    Adding Inventory

--- Assets/Scripts/Resources/InventoryManager.cs
+++ Assets/Scripts/Resources/InventoryManager.cs
@@ -21,11 +21,58 @@ namespace UNetUI.Resources
 
         #endregion Singleton
 
-        public List<Item> items;
+        [Header("Item Details")] public Image itemImage;
+        public Text itemName;
+        public Text itemDescription;
+        public Text damageText;
+        public Text defenceText;
+        public Text strengthText;
+        public Text intelText;
+        public Text agilityText;
+
+        [Header("Borders")] public Sprite defaultBorder;
+        public Sprite selectedBorder;
+
+        [Header(("Inventory Items"))] public List<Item> items;
         public GameObject itemPrefab;
         public RectTransform inventoryHolder;
 
-        private void Start() => CreateAndSetInventoryItems();
+        private List<InventoryItem> _items;
+
+        private InventoryItem _itemSelected;
+
+        private InventoryItem ItemSelected
+        {
+            get { return _itemSelected; }
+            set
+            {
+                _itemSelected = value;
+                UpdateUiWithItemSelected();
+            }
+        }
+
+        private void Start()
+        {
+            _items = new List<InventoryItem>();
+
+            CreateAndSetInventoryItems();
+            ClearItemSelected();
+        }
+
+        private void ClearItemSelected()
+        {
+            ItemSelected = null;
+
+            itemImage.sprite = null;
+            itemName.text = "";
+            itemDescription.text = "";
+
+            damageText.text = "";
+            defenceText.text = "";
+            strengthText.text = "";
+            intelText.text = "";
+            agilityText.text = "";
+        }
 
         private void CreateAndSetInventoryItems()
         {
@@ -35,9 +82,54 @@ namespace UNetUI.Resources
                 itemInstance.transform.SetParent(inventoryHolder);
                 itemInstance.transform.localScale = Vector3.one;
 
-                itemInstance.GetComponent<Image>().sprite = item.icon;
-                itemInstance.transform.GetChild(0).GetComponent<Text>().text = item.itemName;
+                Image inventoryItemImage = itemInstance.transform.GetChild(0).GetComponent<Image>();
+                Image inventoryItemBorder = itemInstance.GetComponent<Image>();
+                Text inventoryItemName = itemInstance.transform.GetChild(1).GetComponent<Text>();
+
+                InventoryItem newItem = new InventoryItem
+                {
+                    item = item,
+                    itemBorder = inventoryItemBorder
+                };
+
+                inventoryItemImage.sprite = item.icon;
+                inventoryItemName.text = item.itemName;
+                inventoryItemBorder.sprite = defaultBorder;
+
+                _items.Add(newItem);
+                itemInstance.GetComponent<Button>().onClick.AddListener(() => InventoryItemClicked(newItem));
             }
+        }
+
+        private void UpdateUiWithItemSelected()
+        {
+            foreach (InventoryItem inventoryItem in _items)
+            {
+                if (inventoryItem == ItemSelected)
+                {
+                    inventoryItem.itemBorder.sprite = selectedBorder;
+
+                    itemImage.sprite = inventoryItem.item.icon;
+                    itemName.text = inventoryItem.item.itemName;
+                    itemDescription.text = inventoryItem.item.description;
+
+                    damageText.text = $"Damage: {inventoryItem.item.damage}";
+                    defenceText.text = $"Defence: {inventoryItem.item.defence}";
+                    strengthText.text = $"Strength: {inventoryItem.item.strength}";
+                    intelText.text = $"Intel: {inventoryItem.item.intel}";
+                    agilityText.text = $"Agility: {inventoryItem.item.agility}";
+                }
+                else
+                    inventoryItem.itemBorder.sprite = defaultBorder;
+            }
+        }
+
+        private void InventoryItemClicked(InventoryItem item) => ItemSelected = item;
+
+        private class InventoryItem
+        {
+            public Item item;
+            public Image itemBorder;
         }
     }
 }
