cf73f87a5bf16cc8d9623a8c7930c7fc28be8e0f
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-28 07:49:21 +0530

    Adding Scoring System

---
+++ Assets/Scripts/Asteroids/Scene/MainScene/NetworkedHealthManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UNetUI.Asteroids.Scene.MainScene
+{
+    public class NetworkedHealthManager : MonoBehaviour
+    {
+        #region Singleton
+
+        public static NetworkedHealthManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+
+            if (instance != this)
+                Destroy(gameObject);
+        }
+
+        #endregion Singleton
+
+        public Text healthCountText;
+
+        private int _currentHealth;
+
+        private void Start() => _currentHealth = 3;
+
+        public void ReduceHealth()
+        {
+            if (_currentHealth - 1 >= 0)
+                _currentHealth -= 1;
+            
+            UpdateUiWithText();
+        }
+
+        public void AddHealth()
+        {
+            _currentHealth += 1;
+            UpdateUiWithText();
+        }
+
+        private void UpdateUiWithText() => healthCountText.text = $"X {_currentHealth}";
+    }
+}
---
+++ Assets/Scripts/Asteroids/Scene/MainScene/NetworkedScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace UNetUI.Asteroids.Scene.MainScene
+{
+    public class NetworkedScoreManager : NetworkBehaviour
+    {
+        #region Singleton
+
+        public static NetworkedScoreManager instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+
+            if (instance != this)
+                Destroy(gameObject);
+        }
+
+        #endregion Singleton
+
+        public Text scoreDisplay;
+
+        private float _currentScore;
+
+        private void Start() => _currentScore = 0;
+
+        public void AddScore(float scoreAmount)
+        {
+            _currentScore += scoreAmount;
+            scoreDisplay.text = _currentScore.ToString();
+        }
+    }
+}
