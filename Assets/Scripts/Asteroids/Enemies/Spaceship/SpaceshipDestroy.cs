cd969212693d374344e4641dfa28f86005a645fc
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-28 08:57:15 +0530

    Updated Ship Desyroy Animation

--- Assets/Scripts/Asteroids/Enemies/Spaceship/SpaceshipDestroy.cs
+++ Assets/Scripts/Asteroids/Enemies/Spaceship/SpaceshipDestroy.cs
@@ -6,22 +6,42 @@ namespace UNetUI.Asteroids.Enemies.Spaceship
 {
     [RequireComponent(typeof(HealthSetter))]
     [RequireComponent(typeof(ScoreSetter))]
+    [RequireComponent(typeof(Animator))]
     public class SpaceshipDestroy : NetworkBehaviour
     {
+        public float animationTime;
+
         private HealthSetter _healthSetter;
+        private Animator _shipAnimator;
+
+        private static readonly int Dead = Animator.StringToHash("Dead");
 
         private void Start()
         {
             _healthSetter = GetComponent<HealthSetter>();
             _healthSetter.healthZero += DestroySpaceship;
+
+            _shipAnimator = GetComponent<Animator>();
         }
 
         private void DestroySpaceship()
         {
-            if(!isServer)
+            if (!isServer)
                 return;
-            
-            
+
+            _shipAnimator.SetBool(Dead, true);
+            Invoke(nameof(RemoveSpaceship), animationTime);
+
+            RpcDisplayDamageOnClients(true);
         }
+
+        [ClientRpc]
+        private void RpcDisplayDamageOnClients(bool playDeadAnimation)
+        {
+            _shipAnimator.SetBool(Dead, playDeadAnimation);
+            Invoke(nameof(RemoveSpaceship), animationTime);
+        }
+
+        private void RemoveSpaceship() => Destroy(gameObject);
     }
 }
