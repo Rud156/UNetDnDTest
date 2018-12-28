ad93e2256856b0d67cbf0178b7e5c2ab695ac030
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-12-28 18:15:28 +0530

    Adding PowerUps Controller

--- Assets/Scripts/Asteroids/Power Ups/PowerUpsController.cs
+++ Assets/Scripts/Asteroids/Power Ups/PowerUpsController.cs
@@ -1,29 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements.StyleEnums;
 using UnityEngine.Networking;
-using UNetUI.Asteroids.NetworkedData;
-using UNetUI.Asteroids.Networking;
 using UNetUI.Asteroids.Shared;
 using UNetUI.Extras;
 
-namespace UNetUI.Asteroids.Enemies.Asteroid
+namespace UNetUI.Asteroids.Power_Ups
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(ScreenWrapper))]
-    public class AsteroidMovement : NetworkBehaviour
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PowerUpsController : NetworkBehaviour
     {
-        public float launchVelocity;
-        public float rotationSpeed;
+        public float launchVelocity = 3f;
 
-        private Rigidbody2D _asteroidRb;
         private ScreenWrapper _screenWrapper;
+        private Rigidbody2D _powerUpRb;
 
         private void Start()
         {
             _screenWrapper = GetComponent<ScreenWrapper>();
-            _asteroidRb = GetComponent<Rigidbody2D>();
+            _powerUpRb = GetComponent<Rigidbody2D>();
 
             if (!isServer)
                 return;
@@ -33,32 +27,27 @@ namespace UNetUI.Asteroids.Enemies.Asteroid
                 Mathf.Cos(launchAngle * Mathf.Deg2Rad) * launchVelocity,
                 Mathf.Sin(launchAngle * Mathf.Deg2Rad) * launchVelocity
             );
-            _asteroidRb.AddForce(launchVector, ForceMode2D.Impulse);
+            _powerUpRb.AddForce(launchVector, ForceMode2D.Impulse);
         }
 
-        private void Update() => ServerUpdate();
-
-        private void ServerUpdate()
+        private void Update()
         {
             if (!isServer)
                 return;
 
             _screenWrapper.CheckObjectOutOfScreen();
-            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
 
-            float rotationZ = transform.rotation.eulerAngles.z;
             Vector3 position = transform.position;
-
             float percentX = ExtensionFunctions.Map(position.x, _screenWrapper.LeftMostPoint,
                 _screenWrapper.RightMostPoint, -1, 1);
             float percentY = ExtensionFunctions.Map(position.y, _screenWrapper.TopMostPoint,
                 _screenWrapper.BottomMostPoint, 1, -1);
 
-            RpcRemoteClientsUpdate(percentX, percentY, rotationZ);
+            RpcRemoteClientsUpdate(percentX, percentY);
         }
 
         [ClientRpc]
-        private void RpcRemoteClientsUpdate(float percentX, float percentY, float rotationZ)
+        private void RpcRemoteClientsUpdate(float percentX, float percentY)
         {
             if (isServer)
                 return;
@@ -69,9 +58,7 @@ namespace UNetUI.Asteroids.Enemies.Asteroid
                 ExtensionFunctions.Map(percentY, 1, -1,
                     _screenWrapper.TopMostPoint, _screenWrapper.BottomMostPoint)
             );
-
             transform.position = normalizedPosition;
-            transform.rotation = Quaternion.Euler(Vector3.forward * rotationZ);
         }
     }
 }
