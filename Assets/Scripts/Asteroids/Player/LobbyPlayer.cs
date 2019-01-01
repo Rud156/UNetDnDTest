c0f6aa93c5594069a2eb690bacb54637967a0602
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2019-01-01 20:26:29 +0530

    Adding Test Lobby Player

---
+++ Assets/Scripts/Asteroids/Player/LobbyPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UNetUI.Extras;
+
+public class LobbyPlayer : NetworkLobbyPlayer
+{
+    public Button joinButton;
+    public Text playerName;
+    public Text joinButtonText;
+
+    private Transform _lobbyPlayerHolder;
+
+    public override void OnClientEnterLobby()
+    {
+        base.OnClientEnterLobby();
+
+        _lobbyPlayerHolder = GameObject.FindGameObjectWithTag(TagManager.LobbyPlayerHolder)?.transform;
+        transform.SetParent(_lobbyPlayerHolder);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        if (isLocalPlayer)
+            SetupLocalPlayer();
+        else
+            SetupOtherPlayer();
+    }
+
+    // Called From External Button
+    public void OnJoinButtonClick() => SendReadyToBeginMessage();
+
+    private void SetupLocalPlayer()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        playerName.text = "Player";
+        joinButton.enabled = true;
+        joinButtonText.text = "JOIN";
+    }
+
+    private void SetupOtherPlayer()
+    {
+        playerName.text = "Other Player";
+        joinButton.enabled = false;
+        joinButtonText.text = "...";
+    }
+}
---
+++ Assets/Scripts/Asteroids/Scene/_Home/LobbyManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace UNetUI.Asteroids.Scene._Home
+{
+    public class LobbyManager : NetworkLobbyManager
+    {
+        #region Singleton
+
+        private static LobbyManager _instance;
+
+        private void Awake()
+        {
+            if (_instance == null)
+                _instance = this;
+
+            if (_instance != this)
+                Destroy(gameObject);
+        }
+
+        #endregion Singleton
+
+        public GameObject lobby;
+        public GameObject mainMenu;
+
+        private void Start()
+        {
+            lobby.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+
+        public override void OnStartHost()
+        {
+            base.OnStartHost();
+
+            lobby.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+
+        public override void OnStopHost()
+        {
+            base.OnStopHost();
+
+            lobby.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+}
